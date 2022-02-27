06e9a67bfdd97b19b75fc22a9832097815f38821
N1GHTM4R3 <84954945+IN1GHTM4R3@users.noreply.github.com>
2022-02-27 10:35:26 -0800

    Updated Zones & Coords (Meth & Coke)

--- config.lua
+++ config.lua
@@ -32,7 +32,9 @@ Config.CircleZones = {
 	ChemicalsConvertionMenu = {coords = vector3(3536.71, 3662.63, 28.12), name = ('Chemicals Process'), radius = 100.0},
 
 	CokeField = {coords = vector3(2806.5, 4774.46, 46.98), name = ('Coke'), radius = 100.0},
-	CokeProcessing = {coords = vector3(-239.35, -1515.93, 33.38), name = ('Coke Process'), radius = 100.0},
+	CokeProcessing = {coords = vector3(1087.14, -3195.31, -38.99), name = ('Coke Process'), radius = 20.0}, 
+	CokePowder = {coords = vector3(1092.9, -3196.65, -38.99), name = ('Powder Cutting'), radius = 20.0},--vector3(1092.9, -3196.65, -38.99)
+	CokeBrick = {coords = vector3(1099.57, -3194.35, -38.99), name = ('Brick Up Packages'), radius = 20.0},--vector3(1099.57, -3194.35, -38.99)
 	
 	HeroinField = {coords = vector3(-2339.15, -54.32, 95.05), name = ('Heroin'), radius = 100.0},
 	HeroinProcessing = {coords = vector3(1384.63, -2080.22, 52.61), name = ('Heroin Process'), radius = 100.0},
@@ -40,4 +42,32 @@ Config.CircleZones = {
 	lsdProcessing = {coords = vector3(2503.84, -428.11, 92.99), name = ('LSD process'), radius = 100.0},
 
 	thionylchlorideProcessing = {coords = vector3(-679.59, 5800.46, 17.33), name = ('Thi Clo Process'), radius = 100.0},
+}
+
+
+Config.MethLab = {
+	["enter"] = {
+        coords = vector4(-1187.17, -446.24, 43.91, 306.59),
+    },
+    ["exit"] = {
+        coords = vector4(969.57, -147.07, -46.4, 267.52),  --vector3(969.57, -147.07, -46.4)
+    },
+}
+
+Config.CokeLab = {
+	["enter"] = {
+        coords = vector4(813.21, -2398.69, 23.66, 171.51), --vector3(813.21, -2398.69, 23.66)
+    },
+    ["exit"] = {
+        coords = vector4(1088.68, -3187.68, -38.99, 176.04), -- GTA DLC Biker Cocaine Lab -- vector3(1088.68, -3187.68, -38.99)
+    },
+}
+
+Config.WeedLab = {
+	--[[["enter"] = {
+        coords = vector4(813.21, -2398.69, 23.66, 171.51), --vector3(813.21, -2398.69, 23.66)
+    },
+    ["exit"] = {
+        coords = vector4(1088.68, -3187.68, -38.99, 176.04), -- GTA DLC Biker Cocaine Lab -- vector3(1088.68, -3187.68, -38.99)
+    },]]--
 }
