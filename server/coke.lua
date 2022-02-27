38b6414994deca2fb4631280473a4879331d1ad4
N1GHTM4R3 <84954945+IN1GHTM4R3@users.noreply.github.com>
2022-02-27 10:34:49 -0800

    Updated Coke & Meth Processing

--- server/coke.lua
+++ server/coke.lua
@@ -14,12 +14,40 @@ RegisterServerEvent('qb-drugtrafficking:processCocaLeaf')
 AddEventHandler('qb-drugtrafficking:processCocaLeaf', function()
 	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
+	local count = math.random(2,7)
 
 	Player.Functions.RemoveItem('coca_leaf', 1)
-	Player.Functions.AddItem('coke', 1)
+	Player.Functions.AddItem('coke', count)
 	TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['coca_leaf'], "remove")
-	TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['coke'], "add")
+	TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['coke'], "add", count)
 	TriggerClientEvent('QBCore:Notify', src, 'Cocaine Leafs successfully processed!', "success")
+end)
+
+RegisterServerEvent('qb-drugtrafficking:processCocaPowder')
+AddEventHandler('qb-drugtrafficking:processCocaPowder', function()
+	local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+	local count = math.random(1,2)
+
+	Player.Functions.RemoveItem('coke', 10)
+	Player.Functions.RemoveItem('bakingsoda', 5)
+	Player.Functions.AddItem('coke_small_brick', count)
+	TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['coke'], "remove", 10)
+	TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['bakingsoda'], "remove", 5)
+	TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['coke_small_brick'], "add", count)
+	TriggerClientEvent('QBCore:Notify', src, 'Cocaine Powder successfully Cut!', "success")
+end)
+
+RegisterServerEvent('qb-drugtrafficking:processCocaBrick')
+AddEventHandler('qb-drugtrafficking:processCocaBrick', function()
+	local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+
+	Player.Functions.RemoveItem('coke_small_brick', 4)
+	Player.Functions.AddItem('coke_brick', 1)
+	TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['coke_small_brick'], "remove", 4)
+	TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items['coke_brick'], "add", 1)
+	TriggerClientEvent('QBCore:Notify', src, 'Cocaine Brick Pressed for Distro!', "success")
 end)
 
 function CancelProcessing(playerId)
@@ -33,3 +61,4 @@ RegisterServerEvent('qb-drugtrafficking:cancelProcessing')
 AddEventHandler('qb-drugtrafficking:cancelProcessing', function()
 	CancelProcessing(source)
 end)
+
